ea13815084c42f5cb4922de92e5e23e775e95542
Gouri Katre <gourikatre28@gmail.com>
2025-09-06 02:23:15 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (87.49%) - LeetSync

---
+++ 165-compare-version-numbers/compare-version-numbers.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int compareVersion(string version1, string version2) {
+        int v1= version1.length();
+        int v2= version2.length();
+        int i=0,j=0;
+        while(i<v1 || j<v2){
+            int n1=0;
+            int n2=0;
+
+            while(i<v1 && version1[i]!='.'){
+                n1 = n1*10 +  (version1[i++]-'0');
+            }
+             while(j<v2 && version2[j]!='.'){
+                n2 = n2*10 +  (version2[j++]-'0');
+            }
+
+            if(n1>n2) return 1;
+            if(n1<n2) return -1;
+
+            i++;
+            j++;
+        }
+        return 0;
+    }
+};
